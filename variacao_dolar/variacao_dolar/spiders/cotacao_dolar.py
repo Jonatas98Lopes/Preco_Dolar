5c0fc40ee9469255d199075990bcedfc2a69b239
Jonatas Lopes de Sousa <jonatas.lopes98@outlook.com>
2023-08-24 22:37:35 -0300

    Leitura do arquivo contatos com os e-mails destino

--- variacao_dolar/variacao_dolar/spiders/cotacao_dolar.py
+++ variacao_dolar/variacao_dolar/spiders/cotacao_dolar.py
@@ -6,3 +6,10 @@ arquivo_crendenciais_acesso = 'C:\\Users\\jonat\\Documents\\GitHub\\' \
   'credenciais_acesso.txt'
 with open(arquivo_crendenciais_acesso, 'r') as arquivo:
      EMAIL_USER, EMAIL_PASSWORD = arquivo.read().split('\n')
+
+
+arquivo_contatos = 'C:\\Users\\jonat\\Documents\\GitHub\\' \
+    'Preco-Do_Dolar\\variacao_dolar\\variacao_dolar\\spiders\\utils\\' \
+    'contatos.txt'
+with open(arquivo_contatos, 'r') as arquivo:
+    contatos = arquivo.readlines()
